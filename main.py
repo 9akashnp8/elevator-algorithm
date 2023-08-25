0e077e6651b055c078737c853efda21396d78e60
Akash NP <akashnp1998@gmail.com>
2023-08-25 17:13:36 +0400

    feat: refactor code, implement basic elevator functionality
    remove async code as not required for now

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-import asyncio
+import time
+import logging
 
+logging.basicConfig(
+    level=logging.NOTSET,
+    format='%(levelname)s:%(message)s'
+)
+logger = logging.getLogger()
 FLOORS = [i for i in range(10)]
 
 class Elevator():
     max_weight: int
     max_heads: int
     is_running: bool
-    curr_floor: int = 10
-    destination_floor: int = 20
+    curr_floor: int = 0
+    destination_floor: int
 
     def __init__(
         self,
         max_weight: int,
-        max_heads: int,
-        is_running: bool = False
+        max_heads: int
     ) -> None:
         self.max_weight = max_weight
         self.max_heads = max_heads
-        self.is_running = is_running
     
-    async def go_to_floor(self, floor):
+    def go_to_floor(self, floor):
+        loop_increment = direction = 1
+        if floor < self.curr_floor: loop_increment = direction -1
         self.destination_floor = floor
-        print(f"Moving to floor: {self.destination_floor}")
-        for i in range(self.curr_floor, self.destination_floor):
-            await asyncio.sleep(5)
-            self.curr_floor = i
-            print("Currently on floor: ", self.curr_floor)
-        print(f"Moving complete, current floor: {self.curr_floor}")
+        logger.info(f"currently on: {self.curr_floor}, moving to {self.destination_floor}")
 
-    async def go_up(self):
-        asyncio.create_task(self.go_to_floor(max(FLOORS)))
-    
-    async def go_down(self):
-        asyncio.create_task(self.go_to_floor(min(FLOORS)))
-    
-    def run(self):
-        print("RUNNING")
-        asyncio.run(self.go_up())
-        print("COMPLETE")
+        for i in range(self.curr_floor, self.destination_floor+loop_increment, direction):
+            time.sleep(1)
+            self.curr_floor = i
+            logger.info(f"currently on: {self.curr_floor}")
+        
+        logger.info("movement complete")
+
+def main():
+    elevator = Elevator(1200, 10)
+    while True:
+        destination_floor = int(input("Input Destination Floor: "))
+        elevator.go_to_floor(destination_floor)
+
+if __name__ == '__main__':
+    main()
 
