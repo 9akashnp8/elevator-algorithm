e461d844be1ea549b436ce04e6b1086da820dbc5
Akash NP <akashnp1998@gmail.com>
2023-08-26 19:23:15 +0400

    fix: reset react setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { Theme } from '@radix-ui/themes';
-import '@radix-ui/themes/styles.css';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Theme appearance="dark" accentColor="jade" grayColor="sage">
-      <App />
-    </Theme>
+    <App />
   </React.StrictMode>,
 )
